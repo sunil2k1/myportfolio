5c4f13dc7f05cf2014be2d30869773a5bfa9d837
Sunil2k1 <sunilkpr2k1@gmail.com>
2021-02-06 19:25:58 +0530

    nav bar updated

--- Portfolio/portfolio/src/components/my-navbar/my-navbar.component.jsx
+++ Portfolio/portfolio/src/components/my-navbar/my-navbar.component.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import { Nav, Button } from "react-bootstrap";
 import "./my-navbar.styles.css";
-
+import { bubble as Menu } from 'react-burger-menu';
 class MyNavbar extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     window.onscroll = function() {scrollFunction()};
 
@@ -16,12 +16,53 @@ class MyNavbar extends Component {
         document.getElementById("navbar").style.backgroundColor = "transparent";
       }
     }
+
+    this.state = {
+      width: window.innerWidth,
+    };
   }
-
-
+  
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+  
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+  
   render() {
-    return (
-      <div >
+    const { width } = this.state;
+    const isMobile = width <= 500;
+    // the rest is the same...
+  
+    if (isMobile) {
+      return (
+        <div >
+        <Menu id="navbar">
+        <a>
+        <Nav.Link className="skilllogo" href="https://sunil2k1.github.io/myportfolio/#/home">Home</Nav.Link>
+        </a>
+        <a>
+        <Nav.Link className="skilllogo" href="https://sunil2k1.github.io/myportfolio/#/skills">Skills</Nav.Link>  
+        </a>
+        <a >
+        <Nav.Link className="skilllogo" href="https://sunil2k1.github.io/myportfolio/#/projects">Projects</Nav.Link>  
+        </a>
+        <a>
+        <Nav.Link className="skilllogo" href="https://drive.google.com/file/d/1xi9HnXnFqgH3IygtzbuYnOx1l-ZXM2hY/view?usp=sharing ">Resume</Nav.Link>   
+        </a>
+        </Menu>
+        </div>
+      );
+    } else {
+      return (
+        <div >
         <Navbar
           fixed="top" 
           variant="dark"
@@ -42,9 +83,16 @@ class MyNavbar extends Component {
           </div>
         </Navbar>
       </div>
-    );
+      );
+    }
   }
 }
+
+
+
+ 
+
+
 
 export default MyNavbar;
 
